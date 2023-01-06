057a1f2c1878fb53ba3795aeaee8c90567d07c15
jordanscher <scherjo@uw.edu>
2023-01-05 21:46:27 -0800

    Add authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Authenticator } from "@aws-amplify/ui-react";
+import { AmplifyUser } from '@aws-amplify/ui';
+
+function isManager(user: AmplifyUser | undefined): boolean {
+  const groups = user?.getSignInUserSession()?.getAccessToken().payload['cognito:groups'];
+  return Boolean(groups?.includes('managers'));
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator
+      className="App"
+      hideSignUp={true}
+    >
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <h1>Manager Permissions: {String(isManager(user))}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
